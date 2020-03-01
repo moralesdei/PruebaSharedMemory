0cdecdcb726179ef9f55d697a007d81dc8ce02da
Andres Morales <moralesdei@protonmail.com>
2020-03-01 18:53:11 -0500

    hardware andiendo padding

--- Matmul/device/mult.cl
+++ Matmul/device/mult.cl
@@ -1,6 +1,7 @@
 
 #include "../host/inc/matrixMult.h"
 
+// kernel encargado de la multiplicacion de las matrices.
 __kernel
 __attribute((reqd_work_group_size(BLOCK_SIZE,BLOCK_SIZE,1)))
 __attribute((num_simd_work_items(4)))
@@ -11,21 +12,20 @@ void Matmul(const __global float *m1r,
 						__global float *restrict rer,
 						__global float *restrict rei,
 						const int fil_m1,
-						const int c1f2)
-{
+						const int c1f2){
 
 	__local float m1r_local[BLOCK_SIZE][BLOCK_SIZE];
 	__local float m1i_local[BLOCK_SIZE][BLOCK_SIZE];
 	__local float m2r_local[BLOCK_SIZE][BLOCK_SIZE];
 	__local float m2i_local[BLOCK_SIZE][BLOCK_SIZE];
 
-	// Block index
-	int block_i = get_group_id(0);
-	int block_j = get_group_id(1);
-
 // Local ID index (offset within a block)
 	int local_i = get_local_id(0);
 	int local_j = get_local_id(1);
+
+	// Block index
+	int global_i = BLOCK_SIZE * get_group_id(0) + local_i;
+	int global_j = BLOCK_SIZE * get_group_id(1) + local_j;
 
 	float tmpr = 0.0f;
 	float tmpi = 0.0f;
@@ -37,10 +37,10 @@ void Matmul(const __global float *m1r,
 		const int tiledRow = BLOCK_SIZE * t + local_i;
 		const int tiledCol = BLOCK_SIZE * t + local_j;
 
-		m1r_local[local_i][local_j] = m1r[tiledCol*fil_m1+block_i];
-		m1i_local[local_i][local_j] = m1i[tiledCol*fil_m1+block_i];
-		m2r_local[local_i][local_j] = m2r[block_j*c1f2+tiledRow];
-		m2i_local[local_i][local_j] = m2i[block_j*c1f2+tiledRow];
+		m1r_local[local_j][local_i] = m1r[tiledCol*fil_m1+global_i];
+		m1i_local[local_j][local_i] = m1i[tiledCol*fil_m1+global_i];
+		m2r_local[local_j][local_i] = m2r[global_j*c1f2+tiledRow];
+		m2i_local[local_j][local_i] = m2i[global_j*c1f2+tiledRow];
 
 		barrier(CLK_LOCAL_MEM_FENCE);
 
@@ -53,11 +53,12 @@ void Matmul(const __global float *m1r,
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}
 
-	rer[get_global_id(1) * get_global_size(0) + get_global_id(0)] = tmpr * (-1);
-	rei[get_global_id(1) * get_global_size(0) + get_global_id(0)] = tmpi * (-1);
+	rer[global_j * fil_m1 + global_i] = tmpr;
+	rei[global_j * fil_m1 + global_i] = tmpi * (-1);
 
 }
 
+// Kernel encargado de anadir el padding
 __kernel
 __attribute((reqd_work_group_size(BLOCK_SIZE,BLOCK_SIZE,1)))
 __attribute((num_simd_work_items(4)))
@@ -66,29 +67,27 @@ void paddingAddZeroes(const int P,
 											const __global float* input,
 											const int P_XL,
 											const int Q_XL,
-											__global float* output) {
+											__global float *restrict output) {
 
-		// Thread identifiers
-		const int tx = get_group_id(0)*BLOCK_SIZE + get_local_id(0); // 0..P_XL
-		const int ty = get_group_id(1)*BLOCK_SIZE + get_local_id(1); // 0..Q_XL
+	// Thread identifiers
+	const int tx = get_group_id(0)*BLOCK_SIZE + get_local_id(0); // 0..P_XL
+	const int ty = get_group_id(1)*BLOCK_SIZE + get_local_id(1); // 0..Q_XL
 
-		// Check whether we are within bounds of the XL matrix
-		if (tx < P_XL && ty < Q_XL) {
-
-				// Copy the input or pad a zero
-				float value;
-				if (tx < P && ty < Q) {
-						value = input[ty*P + tx];
-				}
-				else {
-						value = 0.0f;
-				}
-
-				// Store the result
-				output[ty*P_XL + tx] = value;
+		// Copy the input or pad a zero
+		float value;
+		if (tx < P && ty < Q) {
+					value = input[ty*P + tx];
 		}
+		else{
+			value = 0.0f;
+		}
+
+		// Store the result
+		output[ty*P_XL + tx] = value;
+
 }
 
+// Kernel encargado de remover el padding.
 __kernel
 __attribute((reqd_work_group_size(BLOCK_SIZE,BLOCK_SIZE,1)))
 __attribute((num_simd_work_items(4)))
@@ -97,17 +96,14 @@ void paddingRemoveZeroes(const int P,
 													const __global float* input,
 													const int P_XL,
 													const int Q_XL,
-													__global float* output) {
+													__global float *restrict output) {
 
-		// Thread identifiers
-		const int tx = get_group_id(0)*BLOCK_SIZE + get_local_id(0); // 0..P_XL
-		const int ty = get_group_id(1)*BLOCK_SIZE + get_local_id(1); // 0..Q_XL
+	// Thread identifiers
+	const int tx = get_group_id(0)*BLOCK_SIZE + get_local_id(0); // 0..P_XL
+	const int ty = get_group_id(1)*BLOCK_SIZE + get_local_id(1); // 0..Q_XL
 
-		// Check whether we are within bounds of the XL matrix
-		if (tx < P_XL && ty < Q_XL) {
+	if (tx < P && ty < Q) {
+		output[ty*P + tx] = input[ty*P_XL + tx];
+	}
 
-				if (tx < P && ty < Q) {
-						output[ty*P + tx] = input[ty*P_XL + tx];
-				}
-		}
 }
--- Matmul/host/src/MultiplicacionMatrices.cpp
+++ Matmul/host/src/MultiplicacionMatrices.cpp
@@ -42,102 +42,172 @@ static cl_platform_id platform = NULL;
 static cl_device_id device = NULL;
 static cl_context context = NULL;
 static cl_command_queue queue = NULL;
-static cl_kernel kernel = NULL;
+static cl_kernel kernelM = NULL;
+static cl_kernel kernelA = NULL;
+static cl_kernel kernelR = NULL;
 static cl_program program = NULL;
 static cl_mem buff_m1r, buff_m1i, buff_m2r, buff_m2i, buff_rer, buff_rei;
-static void *m1rH, *m1iH, *m2rH, *m2iH, *rerH, *reiH;
+static cl_mem buff_m1r_pad, buff_m1i_pad, buff_m2r_pad, buff_m2i_pad, buff_rer_pad, buff_rei_pad;
+static float *m1rH, *m1iH, *m2rH, *m2iH, *rerH, *reiH;
+static size_t local[2] = {BLOCK_SIZE, BLOCK_SIZE};
+
+static cl_int status;
 
 bool init();
 void cleanup();
 double get_wall_time();
+void addpad(int, int, int, int, cl_mem, int, int, cl_mem);
+void removepad(int, int, int, int, cl_mem, int, int, cl_mem);
+void multiply(int, int, cl_mem, cl_mem, cl_mem, cl_mem, cl_mem, cl_mem, int, int);
 
 int main(int argc, char *argv[]){
-
-
-	cl_int status;
 
 	if(!init()){
 			return -1;
 	}
 
-	const size_t fil_m1 = atoi(argv[1]);
-	const size_t col_m2 = atoi(argv[2]);
-	const size_t c1f2 = atoi(argv[3]);
+	int pad_c1f2, pad_col_m2, pad_fil_m1;
+	int col_m2, fil_m1, c1f2;
+	fil_m1 = atoi(argv[1]);
+	col_m2 = atoi(argv[2]);
+	c1f2 = atoi(argv[3]);
 
 	int shmid_m1r,shmid_m2r, shmid_m1i, shmid_m2i, shmid_rer, shmid_rei;
+  pad_fil_m1 = fil_m1;
+  pad_col_m2 = col_m2;
+	pad_c1f2 = c1f2;
 
-	shmid_m1i = shmget(SHM_KEY_M1R, sizeof(float)*fil_m1*c1f2, 0644|IPC_CREAT);
-	shmid_m1r = shmget(SHM_KEY_M1I, sizeof(float)*fil_m1*c1f2, 0644|IPC_CREAT);
-	shmid_m2i = shmget(SHM_KEY_M2R, sizeof(float)*c1f2*col_m2, 0644|IPC_CREAT);
-	shmid_m2r = shmget(SHM_KEY_M2I, sizeof(float)*c1f2*col_m2, 0644|IPC_CREAT);
+	bool flag_m1 = false;
+	bool flag_m2 = false;
+	bool flag_m = false;
+
+	for(int i=0;i<BLOCK_SIZE-1;i++){
+
+			if(pad_fil_m1%20 != 0)
+				{
+					pad_fil_m1++;
+					flag_m1 = true;
+				}
+			if(pad_col_m2%20 != 0)
+				{
+					pad_col_m2++;
+					flag_m2 = true;
+				}
+			if(pad_c1f2%20 != 0)
+				{
+					pad_c1f2++;
+					flag_m1 = true;
+					flag_m2 = true;
+				}
+	}
+
+	shmid_m1r = shmget(SHM_KEY_M1R, sizeof(float)*fil_m1*c1f2, 0644|IPC_CREAT);
+	shmid_m1i = shmget(SHM_KEY_M1I, sizeof(float)*fil_m1*c1f2, 0644|IPC_CREAT);
+	shmid_m2r = shmget(SHM_KEY_M2R, sizeof(float)*c1f2*col_m2, 0644|IPC_CREAT);
+	shmid_m2i = shmget(SHM_KEY_M2I, sizeof(float)*c1f2*col_m2, 0644|IPC_CREAT);
 	shmid_rer = shmget(SHM_KEY_RER, sizeof(float)*fil_m1*col_m2, 0644|IPC_CREAT);
 	shmid_rei = shmget(SHM_KEY_REI, sizeof(float)*fil_m1*col_m2, 0644|IPC_CREAT);
 
-
-	m1rH = shmat(shmid_m1r, NULL, 0);
-	m1iH = shmat(shmid_m1i, NULL, 0);
-	m2rH = shmat(shmid_m2r, NULL, 0);
-	m2iH = shmat(shmid_m2i, NULL, 0);
-	rerH = shmat(shmid_rer, NULL, 0);
-	reiH = shmat(shmid_rei, NULL, 0);
-
-	//size_t a;
-	//status = clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(cl_uint), &a, NULL);
-	//printf("%zd\n", a);
+	m1rH = (float *)shmat(shmid_m1r, NULL, 0);
+	m1iH = (float *)shmat(shmid_m1i, NULL, 0);
+	m2rH = (float *)shmat(shmid_m2r, NULL, 0);
+	m2iH = (float *)shmat(shmid_m2i, NULL, 0);
+	rerH = (float *)shmat(shmid_rer, NULL, 0);
+	reiH = (float *)shmat(shmid_rei, NULL, 0);
 
 	double wall0 = get_wall_time();
 
 	// Create buffers
-	buff_m1r = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float) * fil_m1 * c1f2, m1rH, &status);
+	buff_m1r = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float)*fil_m1*c1f2, (void*)m1rH, &status);
 	checkError(status, "Failed to create buffer");
-	buff_m1i = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float) * fil_m1 * c1f2, m1iH, &status);
+	buff_m1i = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float)*fil_m1*c1f2, (void*)m1iH, &status);
 	checkError(status, "Failed to create buffer");
-	buff_m2r = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float) * c1f2 * col_m2, m2rH, &status);
+	buff_m2r = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float)*c1f2*col_m2, (void*)m2rH, &status);
 	checkError(status, "Failed to create buffer");
-	buff_m2i = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float) * c1f2 * col_m2, m2iH, &status);
+	buff_m2i = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR, sizeof(float)*c1f2*col_m2, (void*)m2iH, &status);
 	checkError(status, "Failed to create buffer");
-	buff_rer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * fil_m1 * col_m2, NULL, &status);
+	buff_rer = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*fil_m1*col_m2, NULL, &status);
 	checkError(status, "Failed to create buffer");
-	buff_rei = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * fil_m1 * col_m2, NULL, &status);
+	buff_rei = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*fil_m1*col_m2, NULL, &status);
 	checkError(status, "Failed to create buffer");
 
-	// Set kernel arguments
-	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&buff_m1r);
-	checkError(status, "Failed to set kernel arg 0");
-	status = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&buff_m1i);
-	checkError(status, "Failed to set kernel arg 1");
-	status = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&buff_m2r);
-	checkError(status, "Failed to set kernel arg 2");
-	status = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void*)&buff_m2i);
-	checkError(status, "Failed to set kernel arg 3");
-	status = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void*)&buff_rer);
-	checkError(status, "Failed to set kernel arg 4");
-	status = clSetKernelArg(kernel, 5, sizeof(cl_mem), (void*)&buff_rei);
-	checkError(status, "Failed to set kernel arg 5");
-	status = clSetKernelArg(kernel, 6, sizeof(cl_mem), (void*)&col_m2);
-	checkError(status, "Failed to set kernel arg 6");
-	status = clSetKernelArg(kernel, 7, sizeof(cl_mem), (void*)&c1f2);
-	checkError(status, "Failed to set kernel arg 7");
 
-	// Configure work set over which the kernel will execute
+	if(flag_m1 && flag_m2)
+		{
+			buff_m1r_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*pad_c1f2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_m1i_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*pad_c1f2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_m2r_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_c1f2*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_m2i_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_c1f2*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rer_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rei_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
 
-	size_t local[2] = {BLOCK_SIZE, BLOCK_SIZE};
-	size_t global[2] = {fil_m1, col_m2};
+			addpad(pad_fil_m1, pad_c1f2, fil_m1, c1f2, buff_m1r, pad_fil_m1, pad_c1f2, buff_m1r_pad);
+			addpad(pad_fil_m1, pad_c1f2, fil_m1, c1f2, buff_m1i, pad_fil_m1, pad_c1f2, buff_m1i_pad);
+			addpad(pad_c1f2, pad_col_m2, c1f2, col_m2, buff_m2r, pad_c1f2, pad_col_m2, buff_m2r_pad);
+			addpad(pad_c1f2, pad_col_m2, c1f2, col_m2, buff_m2i, pad_c1f2, pad_col_m2, buff_m2i_pad);
+			multiply(pad_fil_m1, pad_col_m2, buff_m1r_pad, buff_m1i_pad, buff_m2r_pad, buff_m2i_pad, buff_rer_pad, buff_rei_pad, pad_fil_m1, pad_c1f2);
+			removepad(pad_fil_m1, pad_col_m2, fil_m1, col_m2, buff_rer_pad, pad_fil_m1, pad_col_m2, buff_rer);
+			removepad(pad_fil_m1, pad_col_m2, fil_m1, col_m2, buff_rei_pad, pad_fil_m1, pad_col_m2, buff_rei);
 
-	// Launch the kernel
-	status = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global, local, 0, NULL, NULL);
-	checkError(status, "Failed to launch kernel");
+		}
+	else if(flag_m1)
+		{
+			buff_m1r_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*c1f2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_m1i_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*c1f2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rer_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rei_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*pad_fil_m1*col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+
+
+			addpad(pad_fil_m1, c1f2, fil_m1, c1f2, buff_m1r, pad_fil_m1, c1f2, buff_m1r_pad);
+			addpad(pad_fil_m1, c1f2, fil_m1, c1f2, buff_m1i, pad_fil_m1, c1f2, buff_m1i_pad);
+			multiply(pad_fil_m1, col_m2, buff_m1r_pad, buff_m1i_pad, buff_m2r, buff_m2i, buff_rer_pad, buff_rei_pad, pad_fil_m1, c1f2);
+			removepad(pad_fil_m1, col_m2, fil_m1, col_m2, buff_rer_pad, pad_fil_m1, col_m2, buff_rer);
+			removepad(pad_fil_m1, col_m2, fil_m1, col_m2, buff_rei_pad, pad_fil_m1, col_m2, buff_rei);
+		}
+	else if(flag_m2)
+		{
+			buff_m2r_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*c1f2*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_m2i_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*c1f2*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rer_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*fil_m1*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+			buff_rei_pad = clCreateBuffer(context, CL_MEM_ALLOC_HOST_PTR, sizeof(float)*fil_m1*pad_col_m2, NULL, &status);
+			checkError(status, "Failed to create buffer");
+
+
+			addpad(c1f2, pad_col_m2, c1f2, col_m2, buff_m2r, c1f2, pad_col_m2, buff_m2r_pad);
+			addpad(c1f2, pad_col_m2, c1f2, col_m2, buff_m2i, c1f2, pad_col_m2, buff_m2i_pad);
+			multiply(fil_m1, pad_col_m2, buff_m1r, buff_m1i, buff_m2r_pad, buff_m2i_pad, buff_rer_pad, buff_rei_pad, fil_m1, c1f2);
+			removepad(fil_m1, pad_col_m2, fil_m1, col_m2, buff_rer_pad, fil_m1, pad_col_m2, buff_rer);
+			removepad(fil_m1, pad_col_m2, fil_m1, col_m2, buff_rei_pad, fil_m1, pad_col_m2, buff_rei);
+
+		}
+	else
+		{
+			multiply(fil_m1, col_m2, buff_m1r, buff_m1i, buff_m2r, buff_m2i, buff_rer, buff_rei, fil_m1, c1f2);
+		}
 
 	// Read buffer output
-	status = clEnqueueReadBuffer(queue, buff_rer, CL_TRUE, 0, sizeof(float) * fil_m1 * col_m2, rerH, 0, NULL, NULL);
+	status = clEnqueueReadBuffer(queue, buff_rer, CL_TRUE, 0, sizeof(float) * fil_m1 * col_m2, (void*)rerH, 0, NULL, NULL);
 	checkError(status, "Failed to read buffer");
-	status = clEnqueueReadBuffer(queue, buff_rei, CL_TRUE, 0, sizeof(float) * fil_m1 * col_m2, reiH, 0, NULL, NULL);
+	status = clEnqueueReadBuffer(queue, buff_rei, CL_TRUE, 0, sizeof(float) * fil_m1 * col_m2, (void*)reiH, 0, NULL, NULL);
 	checkError(status, "Failed to read buffer");
 
 	double wall1 = get_wall_time();
-	printf("El wall time es igual a : %f", wall1 - wall0 );
+	printf("\nEl wall time es igual a : %f", wall1 - wall0 );
 
-  cleanup();
+	cleanup();
 
 	shmdt(rerH);
 	shmdt(reiH);
@@ -181,8 +251,32 @@ if(buff_m2i) {
 		clEnqueueUnmapMemObject(queue, buff_rei, reiH, 0, NULL, NULL);
 		clReleaseMemObject(buff_rei);
 	}
-	if(kernel) {
-		clReleaseKernel(kernel);
+	if(buff_m1r_pad) {
+		clReleaseMemObject(buff_m1r_pad);
+	}
+	if(buff_m1i_pad) {
+		clReleaseMemObject(buff_m1i_pad);
+	}
+	if(buff_m2r_pad) {
+		clReleaseMemObject(buff_m2r_pad);
+	}
+	if(buff_m2i_pad) {
+		clReleaseMemObject(buff_m2i_pad);
+	}
+	if(buff_rer_pad) {
+		clReleaseMemObject(buff_rer_pad);
+	}
+	if(buff_rei_pad) {
+		clReleaseMemObject(buff_rei_pad);
+	}
+	if(kernelM) {
+		clReleaseKernel(kernelM);
+	}
+	if(kernelA) {
+		clReleaseKernel(kernelA);
+	}
+	if(kernelR) {
+		clReleaseKernel(kernelR);
 	}
 	if(program) {
 		clReleaseProgram(program);
@@ -196,7 +290,7 @@ if(buff_m2i) {
 }
 
 bool init() {
-	cl_int status;
+
 	platform = findPlatform("Altera");
 
 	if(platform == NULL) {
@@ -231,8 +325,20 @@ bool init() {
 
 	// Create the kernel - name passed in here must match kernel name in the
 	// original CL file, that was compiled into an AOCX file using the AOC tool
-	const char *kernel_name = "Matmul";  // Kernel name, as defined in the CL file
-	kernel = clCreateKernel(program, kernel_name, &status);
+	const char *kernelM_name = "Matmul";  // Kernel name, as defined in the CL file
+	kernelM = clCreateKernel(program, kernelM_name, &status);
+	checkError(status, "Failed to create kernel");
+
+	// Create the kernel - name passed in here must match kernel name in the
+	// original CL file, that was compiled into an AOCX file using the AOC tool
+	const char *kernelA_name = "paddingAddZeroes";  // Kernel name, as defined in the CL file
+	kernelA = clCreateKernel(program, kernelA_name, &status);
+	checkError(status, "Failed to create kernel");
+
+	// Create the kernel - name passed in here must match kernel name in the
+	// original CL file, that was compiled into an AOCX file using the AOC tool
+	const char *kernelR_name = "paddingRemoveZeroes";  // Kernel name, as defined in the CL file
+	kernelR = clCreateKernel(program, kernelR_name, &status);
 	checkError(status, "Failed to create kernel");
 
 	return true;
@@ -245,4 +351,73 @@ double get_wall_time(){
         return 0;
     }
     return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+void addpad(int f, int c, int arg0, int arg1, cl_mem arg2, int arg3, int arg4, cl_mem arg5){
+
+		size_t global[2] = {f, c};
+		// Set kernel arguments add pad
+		status = clSetKernelArg(kernelA, 0, sizeof(int), (void*)&arg0);
+		checkError(status, "Failed to set kernel arg 0");
+		status = clSetKernelArg(kernelA, 1, sizeof(int), (void*)&arg1);
+		checkError(status, "Failed to set kernel arg 1");
+		status = clSetKernelArg(kernelA, 2, sizeof(cl_mem), (void*)&arg2);
+		checkError(status, "Failed to set kernel arg 2");
+		status = clSetKernelArg(kernelA, 3, sizeof(int), (void*)&arg3);
+		checkError(status, "Failed to set kernel arg 3");
+		status = clSetKernelArg(kernelA, 4, sizeof(int), (void*)&arg4);
+		checkError(status, "Failed to set kernel arg 4");
+		status = clSetKernelArg(kernelA, 5, sizeof(cl_mem), (void*)&arg5);
+		checkError(status, "Failed to set kernel arg 5");
+
+		// Launch the kernel add pad
+		status = clEnqueueNDRangeKernel(queue, kernelA, 2, NULL, global, local, 0, NULL, NULL);
+		checkError(status, "Failed to launch kernel");
+	}
+
+void removepad(int f, int c, int arg0, int arg1, cl_mem arg2, int arg3, int arg4, cl_mem arg5){
+
+		size_t global[2] = {f, c};
+		// Set kernel arguments remove pad
+		status = clSetKernelArg(kernelR, 0, sizeof(int), (void*)&arg0);
+		checkError(status, "Failed to set kernel arg 0");
+		status = clSetKernelArg(kernelR, 1, sizeof(int), (void*)&arg1);
+		checkError(status, "Failed to set kernel arg 1");
+		status = clSetKernelArg(kernelR, 2, sizeof(cl_mem), (void*)&arg2);
+		checkError(status, "Failed to set kernel arg 2");
+		status = clSetKernelArg(kernelR, 3, sizeof(int), (void*)&arg3);
+		checkError(status, "Failed to set kernel arg 3");
+		status = clSetKernelArg(kernelR, 4, sizeof(int), (void*)&arg4);
+		checkError(status, "Failed to set kernel arg 4");
+		status = clSetKernelArg(kernelR, 5, sizeof(cl_mem), (void*)&arg5);
+		checkError(status, "Failed to set kernel arg 5");
+
+		// Launch the kernel remove pad
+		status = clEnqueueNDRangeKernel(queue, kernelR, 2, NULL, global, local, 0, NULL, NULL);
+		checkError(status, "Failed to launch kernel");
+}
+void multiply(int f, int c, cl_mem arg0, cl_mem arg1, cl_mem arg2, cl_mem arg3, cl_mem arg4, cl_mem arg5, int arg6, int arg7){
+
+		size_t global[2] = {f, c};
+		// Set kernel arguments multiply
+		status = clSetKernelArg(kernelM, 0, sizeof(cl_mem), (void*)&arg0);
+		checkError(status, "Failed to set kernel arg 0");
+		status = clSetKernelArg(kernelM, 1, sizeof(cl_mem), (void*)&arg1);
+		checkError(status, "Failed to set kernel arg 1");
+		status = clSetKernelArg(kernelM, 2, sizeof(cl_mem), (void*)&arg2);
+		checkError(status, "Failed to set kernel arg 2");
+		status = clSetKernelArg(kernelM, 3, sizeof(cl_mem), (void*)&arg3);
+		checkError(status, "Failed to set kernel arg 3");
+		status = clSetKernelArg(kernelM, 4, sizeof(cl_mem), (void*)&arg4);
+		checkError(status, "Failed to set kernel arg 4");
+		status = clSetKernelArg(kernelM, 5, sizeof(cl_mem), (void*)&arg5);
+		checkError(status, "Failed to set kernel arg 5");
+		status = clSetKernelArg(kernelM, 6, sizeof(int), (void*)&arg6);
+		checkError(status, "Failed to set kernel arg 6");
+		status = clSetKernelArg(kernelM, 7, sizeof(int), (void*)&arg7);
+		checkError(status, "Failed to set kernel arg 7");
+
+		// Launch the kernel multiply
+		status = clEnqueueNDRangeKernel(queue, kernelM, 2, NULL, global, local, 0, NULL, NULL);
+		checkError(status, "Failed to launch kernel");
 }
